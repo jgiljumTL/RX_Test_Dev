3041becc5145712afc6b32f10da2ec540b31e16e
jgiljumTL <jgiljum@thorlabs.com>
2022-05-06 15:31:05 -0400

    -Continous loop to read file name input. -Parses and saves file name list elements as attributes in RX class. -Have method to call an RX object from the list and print all contents -PICKUP on Line 35, len(RX_List) not returning list length which is needed to print all RX objects in the list.
    -Goals:
     -Print all RX objects in RX_List.
     -Build method to read the first line of contents in the file and then store that date in the RX object attributes.
     -Build filter (Method) to filter the objects based on their timestamp stored in the RX object attributes.
     -Get the filter to actually work.

--- creation.py
+++ creation.py
@@ -1,6 +1,10 @@
+import re
+
+RX_list = []
+
 class RX:
 
-    def __init__(self, model, serialnumber, output, BWL, BWH, GC, iteration):
+    def __init__(self, model, serialnumber, output, BWL, BWH, GC, iteration): #constructor
         self.model = model
         self.serialnumber = serialnumber
         self.output = output
@@ -10,11 +14,64 @@ class RX:
         self.iteration = iteration
 #        self.date = date
 
-    def print_contents(self):
-        print self.model
-        print self.serialnumber
-        print self.output
-        print self.BWL
-        print self.BWH
-        print self.GC
-        print self.iteration
+    def __repr__(self):
+        return "This in an RX object"
+
+    def print_contents(self): #may not be needed?
+        print "Model: ", self.model
+        print "Serial Number: ", self.serialnumber
+        print "Output: ", self.output
+        print "Band Width Low: ", self.BWL
+        print "Bandwidh High: ", self.BWH
+        print "Gain Control: ", self.GC
+        print "Test Iteration: ", self.iteration
+
+def show_RX_object(unit):
+    print "Printing RX List......................"
+    unit.print_contents()
+
+def show_RX_objects():
+    print "Printing RX List......................"
+    for unit in len(RX_list): #I left off here! Got stuck after because It cannot get the length of RX_list which is needed for printing
+        RX_list[unit].print_contents()
+
+
+def request_file():
+
+    user_file = 0
+
+    while user_file != "":
+
+        user_file = raw_input("Enter File Name : ") #asks for user input
+
+        if user_file == "": #stops script if nothing has been entered
+            break
+
+        parsedfile = re.split('_', user_file) #parses name into list
+        #sets the list elements equal to variables we can refer to later
+        model = parsedfile[0]
+        serialnumber = parsedfile[1]
+        output = parsedfile[3]
+        BWL = parsedfile[4]
+        BWH = parsedfile[5]
+        GC = parsedfile[6]
+        iteration = parsedfile[7]
+
+        A = model + "-" + serialnumber #creates new RX object name
+
+        A = RX(0, 0, 0, 0, 0, 0, 0)  # better way to do this?
+        #creates new RX object and sets its agruments values according to the variables we defined ealier
+        A.model = model
+        A.serialnumber = serialnumber
+        A.output = output
+        A.BWL = BWL
+        A.BWH = BWH
+        A.GC = GC
+        A.iteration = iteration
+
+        RX_list.append(A) #should add new RX object into the RX list
+
+
+
+
+
--- demo.py
+++ demo.py
@@ -1,4 +1,8 @@
 from creation import *
 
-RX1 = RX("RX25AF","U0132","N","BWL=0.00","BWH=2.70","GC=2.20","00.s2p")
-RX1.print_contents()
+#RX1 = RX("RX25AF","U0132","N","BWL=0.00","BWH=2.70","GC=2.20","00.s2p")
+#RX1.print_contents()
+
+request_file()
+show_RX_object(RX_list[0])
+show_RX_objects()
